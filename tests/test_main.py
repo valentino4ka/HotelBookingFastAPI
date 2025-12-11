0d4849f705c02191d835da839d8feb8a8e917b62
valentino4ka <samurai.samurai.men@gmail.com>
2025-12-11 20:32:19 +0300

    Доработана временная вреия теста

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,10 @@ sys.path.append(str(Path(__file__).parent.parent))
 from fastapi.testclient import TestClient
 from app.main import app
 
-# Для старых версий Starlette используем app=, для новых — позиционный
 try:
-    client = TestClient(app=app)  # Старая версия
+    client = TestClient(app=app)  
 except TypeError:
-    client = TestClient(app)      # Новая версия
-
+    client = TestClient(app)     
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
